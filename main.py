d3a66537d7640f02784db575bc9855652e5e1e4e
BIBIZANA <inikband@gmail.com>
2024-03-30 14:21:40 +0300

    Platform

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ run = True
 class GameSprite(sprite.Sprite):
     def __init__ (self, img, x, y, speed, width, height):
         super().__init__()
-        self.image = transform.scale(image.load(img), width, height)
-        self.speed = 0
+        self.image = transform.scale(image.load(img), (width, height))
+        self.speed = speed
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
@@ -24,10 +24,12 @@ class GameSprite(sprite.Sprite):
 
 class Player(GameSprite):
     def update_left(self):
+        keys = key.get_pressed()
         if keys[K_w] and self.rect.y > 0:
             self.rect.y -= self.speed
         if keys [K_s] and self.rect.y + self.rect.height < SCREENSIZE[1]:
             self.rect.y += self.speed
+
     def update_right(self):
         keys = key.get_pressed()
         if keys[K_UP] and self.rect.y > 0:
@@ -36,13 +38,18 @@ class Player(GameSprite):
             self.rect.y += self.speed
 
 
+platform_left = Player("platform.png",20,350, 4,25,99)
+platform_right = Player("platform.png",660,350,4,25,99)
 
 
 while run:
     for e in event.get():
         if e.type == QUIT:
             run = False
-
     window.fill(BACKCOLOR)
+    platform_left.reset()
+    platform_right.reset()
+    platform_left.update_left()
+    platform_right.update_right()
     display.update()
     timer.tick(60)
